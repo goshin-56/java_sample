89976b64d986ea5ca63dceaa35db68bae4abf7f2
goshin-56 <102846714+goshin->
2022-08-27 16:00:09 +0900

    [Add]Q3_3まで

---
+++ src/random.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class random {
+	public static void main(String[] args) {
+		Random ran = new Random();
+		
+		for (int i=1; i<=20; i++) {
+			int sei = ran.nextInt(10)+1;
+			System.out.println(sei);
+		}
+	}
+}
+
